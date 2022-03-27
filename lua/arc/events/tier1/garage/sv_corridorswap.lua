0b4c3bab1d75b30e019a0cdbb5d7c4700f206d80
Xalalau Xubilozo <xalalau@outlook.com>
2022-03-27 20:36:19 -0300

    Move portal swaping to custom entities lib

--- lua/arc/events/tier1/garage/sv_corridorswap.lua
+++ lua/arc/events/tier1/garage/sv_corridorswap.lua
@@ -32,70 +32,28 @@ local function CreateEvent()
                 sizeX = 1.32,
                 sizeY = 1.5,
                 sizeZ = 1
-            },
+            }
         }
     }
 
-    local portals = {}
-    local plysInMaxArea = {}
-    
-    local garageSwapTrigger = ents.Create("gm13_trigger")
-	garageSwapTrigger:Setup(eventName, "garageSwapTrigger", Vector(-2135.97, -2104.07, -42.68), Vector(-1992.38, -2123.54, -143.97))
+    local startTriggersInfo = {
+        {
+            vecA = Vector(-2135.97, -2104.07, -42.68),
+            vecB = Vector(-1992.38, -2123.54, -143.97),
+            probability = 18
+        }
+    }
 
-    function garageSwapTrigger:StartTouch(ent)
-        if table.Count(plysInMaxArea) > 0 then return end
-        if #portals > 0 then return end
-        if not ent:IsPlayer() then return end
+    local endTriggersInfo = {
+        {
+            vecA = Vector(-2521.72, -2558.23, -255.97),
+            vecB = Vector(-1606.66, -2133.27, -34.27)
+        }
+    }
 
-        for k, portalPair in ipairs(portalInfo) do
-            local portal1 = ents.Create("cgm13_portal")
-            portal1:SetPos(portalPair[1].pos)
-            portal1:Spawn()
-            portal1:SetAngles(portalPair[1].ang)
-            portal1:SetExitSize(Vector(portalPair[1].sizeX, portalPair[1].sizeY, portalPair[1].sizeZ))
-            GM13.Ent:BlockPhysgun(portal1, true)
-            table.insert(portals, portal1)
-    
-            local portal2 = ents.Create("cgm13_portal")
-            portal2:SetPos(portalPair[2].pos)
-            portal2:Spawn()
-            portal2:SetAngles(portalPair[2].ang)
-            portal2:SetExitSize(Vector(portalPair[2].sizeX, portalPair[2].sizeY, portalPair[2].sizeZ))
-            GM13.Ent:BlockPhysgun(portal2, true)
-            table.insert(portals, portal2)
-    
-            portal1:LinkPortal(portal2)
-            portal1.PORTAL_REMOVE_EXIT = true
-            portal2.PORTAL_REMOVE_EXIT = true
-        end
-	end
+    CGM13.Custom:CreatePortalSwap(eventName, portalInfo, startTriggersInfo, endTriggersInfo)
 
-    local garageSwapTriggerMaxArea = ents.Create("gm13_trigger")
-	garageSwapTriggerMaxArea:Setup(eventName, "garageSwapTriggerMaxArea", Vector(-2521.72, -2558.23, -255.97), Vector(-1606.66, -2133.27, -34.27))
-
-    function garageSwapTriggerMaxArea:SartTouch()
-        if ent:IsPlayer() then
-            plysInMaxArea[ent] = nil
-        end
-    end
-
-    function garageSwapTriggerMaxArea:EndTouch(ent)
-        if not ent:IsPlayer() then return end
-
-        plysInMaxArea[ent] = nil
-
-        if table.Count(plysInMaxArea) == 0 and #portals > 0 then
-            for k, portal in ipairs(portals) do
-                if portal:IsValid() then
-                    portal:Remove()
-                end
-            end
-
-            portals = {}
-        end
-	end
-
-	return true
+    return true
 end
 
 GM13.Event:SetCall(eventName, CreateEvent)
