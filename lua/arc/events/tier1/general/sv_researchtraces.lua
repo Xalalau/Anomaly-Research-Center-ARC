0b46afb07848c2ec47d7adf66b53f41e19b251e2
Xalalau Xubilozo <xalalau@outlook.com>
2022-04-08 03:27:06 -0300

    Improv generalResearchTraces even more

--- lua/arc/events/tier1/general/sv_researchtraces.lua
+++ lua/arc/events/tier1/general/sv_researchtraces.lua
@@ -20,105 +20,53 @@ local function CreateProp(model, pos, ang)
 end
 
 local function MainEvent()
-    local propsTab = {
+    local propTab = {
         { -- tunnels entrace near building b
-            pos = {
-                Vector(-5507.6, -3911.9, 262.4),
-                Vector(-5502.3, -3871.7, 262.9),
-                Vector(-5472.4, -3893.4, 263.3),
-                Vector(-5469.7, -3866.9, 262.9),
-                Vector(-5443.9, -3891.1, 262.7)
-            },
-            model = {
-                "random",
-                "random",
-                "random",
-                "random",
-                "box"
-            }
+            { pos = Vector(-5507.6, -3911.9, 262.4) },
+            { pos = Vector(-5502.3, -3871.7, 262.9) },
+            { pos = Vector(-5472.4, -3893.4, 263.3) },
+            { pos = Vector(-5469.7, -3866.9, 262.9) },
+            { pos = Vector(-5443.9, -3891.1, 262.7), isBox = true }
         },
         { -- submarine room
-            pos = {
-                Vector(2002.47, 5689.34, -145.96),
-                Vector(2033.01, 5694.62, -145.96),
-                Vector(2034.34, 5669.50, -145.96),
-                Vector(2010.28, 5648.97, -145.96),
-                Vector(2000.03, 5703.96, -145.96)
-            },
-            model = {
-                "random",
-                "random",
-                "random",
-                "random",
-                "random"
-            }
+            { pos = Vector(2002.47, 5689.34, -145.96) },
+            { pos = Vector(2033.01, 5694.62, -145.96) },
+            { pos = Vector(2034.34, 5669.50, -145.96) },
+            { pos = Vector(2010.28, 5648.97, -145.96) },
+            { pos = Vector(2000.03, 5703.96, -145.96) }
         },
         { -- building c
-            pos = {
-                Vector(-4256.03, 5939.96, -82.96),
-                Vector(-4269.34, 5890.10, -82.96),
-                Vector(-4302.91, 5933.41, -82.96),
-                Vector(-4321.23, 5885.95, -82.96)
-            },
-            model = {
-                "random",
-                "random",
-                "random",
-                "random"
-            }
+            { pos = Vector(-4256.03, 5939.96, -82.96) },
+            { pos = Vector(-4269.34, 5890.10, -82.96) },
+            { pos = Vector(-4302.91, 5933.41, -82.96) },
+            { pos = Vector(-4321.23, 5885.95, -82.96) }
         },
         { -- building a
-            pos = {
-                Vector(1831.96, -2150.68, 1145.03),
-                Vector(1799.65, -2122.03, 1145.03),
-                Vector(1818.87, -2081.78, 1145.03),
-                Vector(1749.92, -2136.60, 1145.03),   
-            },
-            model = {
-                "random",
-                "random",
-                "random",
-                "random"
-            }
+            { pos = Vector(1831.96, -2150.68, 1145.03) },
+            { pos = Vector(1799.65, -2122.03, 1145.03) },
+            { pos = Vector(1818.87, -2081.78, 1145.03) },
+            { pos = Vector(1749.92, -2136.60, 1145.03) }
         },
         { -- garage
-            pos = {
-                Vector(-3215.98, -1903.99, 55.03),
-                Vector(-3215.96, -1829.90, 55.03),
-                Vector(-3158.19, -1847.12, 55.03),
-                Vector(-3143.70, -1903.99, 55.03),
-                Vector(-3191.64, -1881.06, 55.03)
-            },
-            model = {
-                "random",
-                "random",
-                "random",
-                "random",
-                "box"
-            }
+            { pos = Vector(-3215.98, -1903.99, 55.03) },
+            { pos = Vector(-3215.96, -1829.90, 55.03) },
+            { pos = Vector(-3158.19, -1847.12, 55.03) },
+            { pos = Vector(-3143.70, -1903.99, 55.03) },
+            { pos = Vector(-3191.64, -1881.06, 55.03), isBox = true }
         },
         { -- bunker
-            pos = {
-                Vector(-995.93, 1230.89, -527.97),
-                Vector(-972.05, 1175.99, -527.97),
-                Vector(-938.18, 1180.03, -527.97),
-                Vector(-1037.16, 1178.28, -527.97),
-                Vector(-1009.29, 1120.44, -527.97)
-            },
-            model = {
-                "random",
-                "random",
-                "random",
-                "random",
-                "random"
-            }
+            { pos = Vector(-995.93, 1230.89, -527.97) },
+            { pos = Vector(-972.05, 1175.99, -527.97) },
+            { pos = Vector(-938.18, 1180.03, -527.97) },
+            { pos = Vector(-1037.16, 1178.28, -527.97) },
+            { pos = Vector(-1009.29, 1120.44, -527.97) }
         }
     }
     
-    for _, eventTab in ipairs(propsTab) do
-        for k, pos in ipairs(eventTab.pos) do
+    for _, propGroup in ipairs(propTab) do
+        for k, propInfo in ipairs(propGroup) do
             local propMarker = ents.Create("gm13_marker_prop")
-            propMarker:Setup(eventName, eventName .. "PropMarker" .. k .. "_".. tostring(eventTab), pos)
+            propMarker:Setup(eventName, eventName .. "PropMarker" .. k .. "_".. tostring(propInfo), propInfo.pos)
         end
     end
 
@@ -149,17 +97,17 @@ local function MainEvent()
     }
     
     local delay = math.random(90, 160)
-    local eventFadeTime = 1.5
-    local eventMaxTime = delay - eventFadeTime - 1
+    local propsFadeTime = 1.5
+    local propsMaxTime = delay - propsFadeTime - 1
 
-    timer.Create("cgm13_researcherTraces_control", delay, 1, function()
+    timer.Create("cgm13_researcher_traces_control", delay, 1, function()
 		if math.random(1, 100) <= 37 then
             local isVisible = false
-            local event = propsTab[math.random(#propsTab)]
+            local propGroup = propTab[math.random(#propTab)]
 
             for _, ply in ipairs(player.GetHumans()) do
-                for __, pos in ipairs(event.pos) do
-                    if ply:VisibleVec(pos) then
+                for __, propInfo in ipairs(propGroup) do
+                    if ply:VisibleVec(propInfo.pos) then
                         isVisible = true
                         break
                     end
@@ -167,31 +115,25 @@ local function MainEvent()
             end
 
             if not isVisible then
-                for k, pos in ipairs(event.pos) do
-                    local model = event.model[k]
-                    local isBox = model == "box"
-                    local isRandom = model == "random"
-                    model = isBox and "models/props_junk/cardboard_box004a.mdl" or isRandom and junkModels[math.random(#junkModels)]
+                for k, propGroup in ipairs(propGroup) do
+                    local model = propGroup.isBox and "models/props_junk/cardboard_box004a.mdl" or junkModels[math.random(#junkModels)]
+                    local pos = propGroup.pos
+                    local ang = Angle(0, math.random(0, 180), 0)
 
-                    if model then
-                        local ang = Angle(0, math.random(0, 180), 0)
-                        local prop = CreateProp(model, pos, ang)
+                    local prop = CreateProp(model, pos, ang)
 
-                        if prop and prop:IsValid() then
-                            if isBox then
-                                GM13.Ent:SetInvulnerable(prop, true)
-                                prop:Ignite(eventMaxTime) 
-                            end
-
-                            timer.Simple(eventMaxTime, function()
-                                if not prop:IsValid() then return end
-
-                                GM13.Ent:FadeOut(prop, eventFadeTime, function()
-                                    prop:Remove()
-                                end)
-                            end)
-                        end
+                    if propGroup.isBox then
+                        GM13.Ent:SetInvulnerable(prop, true)
+                        prop:Ignite(propsMaxTime) 
                     end
+
+                    timer.Simple(propsMaxTime, function()
+                        if not prop:IsValid() then return end
+
+                        GM13.Ent:FadeOut(prop, propsFadeTime, function()
+                            prop:Remove()
+                        end)
+                    end)
                 end
             end
         end
@@ -201,7 +143,7 @@ local function MainEvent()
 end
 
 local function RemoveEvent()
-    timer.Remove("cgm13_researcherTraces_control")
+    timer.Remove("cgm13_researcher_traces_control")
 end
 
 GM13.Event:SetCall(eventName, MainEvent)
