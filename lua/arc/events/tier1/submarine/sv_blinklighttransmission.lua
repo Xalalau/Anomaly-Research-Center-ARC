1ed02c7fc8f891a04b1f3e8602c0bfcdd6fd6428
Xalalau Xubilozo <xalalau@outlook.com>
2022-04-08 01:40:52 -0300

    Rewrite event of flashing lights near wall holes

--- lua/arc/events/tier1/submarine/sv_blinklighttransmission.lua
+++ lua/arc/events/tier1/submarine/sv_blinklighttransmission.lua
@@ -2,84 +2,44 @@ local eventName = "generalTransmissionFlickerLight"
 
 GM13.Event.Memory.Dependency:SetDependent(eventName, "ratmanReady")
 
-local supported = {
-    ["gmod_light"] = true,
-    ["gmod_lamp"] = true,
-}
+local function CreateEvent()
+    timer.Simple(5, function()
+        for _, sprite in ipairs(ents.FindByClass("gm13_func_sprite")) do
+            if string.find(sprite:GetName(), "wallHole_") then
+                local visiblePoint = sprite:GetRight() * 50
+                visiblePoint = sprite:GetVar("vecCenter") + visiblePoint
 
-local function Flicker(light)
-    if not light or not IsValid(light) or not supported[light:GetClass()] then return end
-    GM13.Light:Blink(light, 2, true)
-end
+                local timerName = "gm13_wall_hole_light_flicker" .. tostring(sprite)
 
-local function SetupFlickerTrigger(evName, entName, veca, vecb, notDetectable, delay, main)
-    local t = ents.Create("gm13_trigger")
-    t:Setup(evName, entName, veca, vecb, notDetectable)
-    if not t or not IsValid(t) then return end
-    function t:StartTouch(ent)
-        if supported[ent:GetClass()] then
+                timer.Create(timerName, 60, 0, function()
+                    if not sprite:IsValid() then
+                        timer.Remove(timerName)
+                        return
+                    end
 
-            Flicker(ent)
-            timer.Create("cgm13_flickertrigger_" .. tostring(ent), delay, 0, function()
-                if not t or not IsValid(t) then timer.Remove("cgm13_flickertrigger_" .. tostring(ent)) return end
-                if not main or not IsValid(main) then timer.Remove("cgm13_flickertrigger_" .. tostring(ent)) return end
+                    for __, ent in ipairs(ents.FindInSphere(sprite:GetPos(), 1000)) do
+                        local supported = {
+                            ["env_projectedtexture"] = true,
+                            ["gmod_light"] = true,
+                            ["gmod_lamp"] = true,
+                            ["light_spot"] = true,
+                            ["light"] = true,
+                            ["classiclight"] = true
+                        }
 
-                Flicker(ent)
-            end)
-
+                        if supported[ent:GetClass()] and ent:VisibleVec(visiblePoint) then
+                            if math.random(1, 100) <= 15 then
+                                local initialState = GM13.Light:IsOn(ent)
+                                GM13.Light:Blink(ent, math.random(1, 2), initialState)
+                            end
+                        end
+                    end 
+                end)
+            end
         end
-    end
-
-    function t:EndTouch(ent)
-        if supported[ent:GetClass()] then
-
-            timer.Remove("cgm13_flickertrigger_" .. tostring(ent))
-        end
-    end
-end
-
-local function GetFlickerTrigger(name)
-    local result = nil
-    for _,v in pairs(ents.FindByClass("gm13_trigger")) do
-        local aName = v:GetVar("entName")
-        if string.EndsWith(aName, "_FlickerTrigger") and aName == name then
-            result = v
-        end
-    end
-    return result
-end
-
-local function CreateEvent() -- Lights will flicker when close to transmission holes
-
-    for _,area in pairs(ents.FindByClass("gm13_trigger")) do
-        local areaName = area:GetVar("entName")
-
-        if string.StartWith(areaName, "radio_") then
-            local pos = area:GetPos()
-            local eName = tostring(area) .. "_FlickerTrigger"
-            SetupFlickerTrigger(eventName, eName, pos + Vector(75,65,82), pos + Vector(-50,-62,-100), false, 2.2, area)
-
-            area:CallOnRemove("cgm13_flickertrigger_control", function()
-                local trigger = GetFlickerTrigger(eName)
-                if IsValid(trigger) then 
-                    GM13.Event:RemoveRenderInfoEntity(trigger)
-                    trigger:Remove() 
-                end
-            end)
-        end
-    end
+    end)
 
     return true
 end
 
-local function RemoveTimers()
-    for _,area in pairs(ents.FindByClass("gm13_trigger")) do
-        local areaName = area:GetVar("entName")
-        if string.EndsWith(areaName, "_FlickerTrigger") then
-            area:Remove()
-        end
-    end
-end
-
 GM13.Event:SetCall(eventName, CreateEvent)
-GM13.Event:SetDisableCall(eventName, RemoveTimers)
